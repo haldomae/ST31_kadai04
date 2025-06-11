54ee571f07312924906539da06a70344509a18d4
haldomae <hal.domae@gmail.com>
2025-06-11 10:09:05 +0900

    入力値をログに出力

--- app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai04/MainActivity.kt
+++ app/src/main/java/com/classnumber_00_domaekazuki/st31_kadai04/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.classnumber_00_domaekazuki.st31_kadai04
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,27 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        // EditTextを取得
+        val input: EditText =
+            findViewById(R.id.input_num)
+        // ボタンを取得
+        val plusButton: Button =
+            findViewById(R.id.plusButton)
+        // ボタンにクリックリスナーを付ける
+        plusButton.setOnClickListener {
+            // ボタンが押されたときにEditTextの文字を取得
+            val inputText = input.text
+
+            // ログ出力
+            // インフォメーション
+            Log.i("DEBUG_LOG", inputText.toString())
+            // デバックのログ
+            Log.d("デバッグ", inputText.toString())
+            // ワーニング
+            Log.w("デバッグのログ", inputText.toString())
+            // エラーログ
+            Log.e("error", inputText.toString())
         }
     }
 }
